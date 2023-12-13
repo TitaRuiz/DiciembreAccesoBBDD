07b24737deae6567ff67dafe8ee05f1164597581
TitaRuiz <ggp2002es@gmail.com>
2023-12-13 19:19:06 +0100

    Final de la clase

--- src/com/corenetworks/presentacion/Main.java
+++ src/com/corenetworks/presentacion/Main.java
@@ -1,5 +1,34 @@
+package com.corenetworks.presentacion;
+
+import com.corenetworks.persistencia.AccesoProducto;
+import com.corenetworks.persistencia.Conexion;
+
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Conexion c1 = new Conexion();
+        try {
+            c1.abriConexion();
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.toString());
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+        AccesoProducto aP1= new AccesoProducto();
+        try {
+            System.out.println(aP1.obtenerUno(5));
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.toString());
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+        try {
+            System.out.println(aP1.obtenerTodos());
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.toString());
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
     }
 }
